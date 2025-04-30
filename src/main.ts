25b12c8d8e996abd56e93b570535c5f25a106ab2
Michael262626 <dikandumichael@gmail.com>
2025-04-30 07:07:54 +0100

    Made some changes

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
 
   app.enableCors({
     origin: [
-      'http://localhost:3001',
+      'http://localhost:3000',
       'https://department-management-frontend-rouge.vercel.app'
     ],
     credentials: true,
@@ -27,6 +27,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(3003);
 }
 bootstrap();
